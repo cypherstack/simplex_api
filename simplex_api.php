2adc9e39920a6d6069d70088809a88800158c038
sneurlax <sneurlax@gmail.com>
2023-01-19 09:29:40 -0600

    update config template

--- simplex_api.php
+++ simplex_api.php
@@ -7,11 +7,14 @@
 
 // Set these values in config.php, which is included and overwrites these below
 // -----------------------------------------------------------------------------
-$API_KEY = 'API Key';
-$PUBLIC_KEY = 'Public Key';
-$WALLET_ID = 'Wallet ID';
-$REFERRER = 'https://example.com/simplex';
-$REFERRAL_IP = '8.8.8.8';
+// $API_KEY = 'API Key';
+// $PUBLIC_KEY = 'Public Key';
+// $WALLET_ID = 'Wallet ID';
+// $REFERRER = 'https://example.com/simplex';
+// $REFERRAL_IP = '8.8.8.8';
+
+// $RETURN_URL_SUCCESS = 'https://example.com/success';
+// $RETURN_URL_FAILURE = 'https://example.com/failure';
 // -----------------------------------------------------------------------------
 include 'config.php';
 
