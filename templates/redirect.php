b1a81ba25facd749cad0e45ba6b69c799c946413
sneurlax <sneurlax@gmail.com>
2023-01-20 09:26:37 -0600

    fail -> failure

--- templates/redirect.php
+++ templates/redirect.php
@@ -2,7 +2,14 @@
 
 // TODO style
 
-function redirect_template($_PAYMENT_ID, $_RETURN_URL_SUCCESS, $_RETURN_URL_FAIL, $_WALLET_ID) {
+function redirect_template($_PAYMENT_ID, $_RETURN_URL_SUCCESS, $_RETURN_URL_FAILURE, $_WALLET_ID) {
+    global $PAYMENT_ID, $RETURN_URL_SUCCESS, $RETURN_URL_FAILURE, $WALLET_ID;
+    $_PAYMENT_ID = isset($_REQUEST['PAYMENT_ID']) ? $_REQUEST['PAYMENT_ID'] : $PAYMENT_ID;
+    $_RETURN_URL_SUCCESS = isset($_REQUEST['RETURN_URL_SUCCESS']) ? $_REQUEST['RETURN_URL_SUCCESS'] : $RETURN_URL_SUCCESS;
+    $_RETURN_URL_FAILURE = isset($_REQUEST['RETURN_URL_FAILURE']) ? $_REQUEST['RETURN_URL_FAILURE'] : $RETURN_URL_FAILURE;
+    $_WALLET_ID = isset($_REQUEST['WALLET_ID']) ? $_REQUEST['WALLET_ID'] : $WALLET_ID;
+    // TODO sanitize $_REQUEST inputs above
+
     $html = <<<EOD
 <html>
     <body>
@@ -11,7 +18,7 @@ function redirect_template($_PAYMENT_ID, $_RETURN_URL_SUCCESS, $_RETURN_URL_FAIL
             <input type="hidden" name="partner" value="$_WALLET_ID">
             <input type="hidden" name="payment_flow_type" value="wallet">
             <input type="hidden" name="return_url_success" value="$_RETURN_URL_SUCCESS">
-            <input type="hidden" name="return_url_fail" value="$_RETURN_URL_FAIL">
+            <input type="hidden" name="return_url_fail" value="$_RETURN_URL_FAILURE">
             <input type="hidden" name="payment_id" value="$_PAYMENT_ID">
         </form>
 
